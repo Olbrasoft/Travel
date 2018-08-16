8442976801743ababbd1ecdbb094cfc315717654
Jiri Tuma <github.com@mail.tj>
2018-08-16 22:07:52 +0200

    Add Mvc.UnitTest

---
+++ Olbrasoft.Travel.Web.Mvc.UnitTest/HomeControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using Olbrasoft.Travel.BusinessLogicLayer;
+using Olbrasoft.Travel.Web.Mvc.Controllers;
+
+namespace Olbrasoft.Travel.Web.Mvc.UnitTest
+{
+    [TestFixture()]
+    public class HomeControllerTest
+    {
+        public HomeController HomeController { get; private set; } 
+
+        [SetUp]
+        public void Setup()
+        {
+            var accommodationsFacade = new Mock<IAccommodationsFacade>();
+            HomeController = new HomeController(accommodationsFacade.Object);
+        }
+
+
+        [Test]
+        public void CreateInstanceTypeOfHomeControllerTest()
+        {
+            //Arrange
+            Controller controller = HomeController;
+
+            //Act
+            var type = typeof(HomeController);
+
+            //Assert
+            Assert.IsInstanceOf(type, controller);
+        }
+
+
+        [Test]
+        public void HomeControllerIndexTest()
+        {
+            //Arrange
+            var controller = HomeController;
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+
+        }
+    }
+}
--- Olbrasoft.Travel.Web.Mvc/Controllers/HomeController.cs
+++ Olbrasoft.Travel.Web.Mvc/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Olbrasoft.Travel.BusinessLogicLayer;
 
 namespace Olbrasoft.Travel.Web.Mvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAccommodationsFacade _accommodationFacade; 
+
+        public HomeController(IAccommodationsFacade accommodationFacade)
+        {
+            _accommodationFacade = accommodationFacade;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
